b262e21222c24e0e8209069765e3de519c68479f
Byroni <bpark8@uncc.edu>
2016-11-10 15:51:48 -0500

    Update

--- page.js
+++ page.js
@@ -2,9 +2,24 @@
  * Created by byron on 11/9/16.
  */
 var dataset = []
-d3.csv("na_player.csv", function(data) {
-	dataset = data.map(function(d) {
-		return [+d["assists"], +d["champLevel"]];
+d3.csv("na_player.json", function(data) {
+
+	dataset = data.map(function (d) {
+		return [+d["id"], +d["assists"], +d["champLevel"], +d["deaths"], +d["doubleKills"], +d[""], +d["firstBloodKill"],
+			+d["firstInhibitorAssist"], +d["firstInhibitorKill"], +d["firstTowerAssist"], +d["firstTowerKill"], +d["goldEarned"],
+			+d["goldSpent"], +d["inhibitorKills"], +d["killingSprees"], +d["kills"], +d["largestCriticalStrike"],
+			+d["largestKillingSpree"], +d["largestMultiKill"], +d["magicDamageDealt"], +d["magicDamageTaken"], +d["minionKill"],
+			+d["neutralMinionsKilled"], +d["neutralMinionsKilledEnemyJungle"], +d["pentaKills"], +d["physicalDamageDealt"],
+			+d["physicalDamageDealtToChampions"], +d["physicalDamageTaken"], +d["quadraKills"], +d["sightWardsBoughtInGame"],
+			+d["totalDamageDealt"], +d["totalDamageDealtToChampions"], +d["totalDamageTaken"], +d["totalHeal"],
+			+d["totalTimeCrowdControlDealt"], +d["totalUnitsHealed"], +d["towerKills"], +d["tripleKills"], +d["trueDamageDealt"]
+			+ d["trueDamageDealtToChampions"], +d["trueDamageTaken"], +d["visionWardsBoughtInGame"], +d["wardsKilled"], +d["wardsPlaced"], +d["winner"]];
 	});
-	console.log(dataset);
+
+
 });
+
+d3.select("body")
+.enter().append("p")
+.text(function (b) {
+	return b + "!"; });
